96926a36be8b56132753d629c5141a6a21f7b623
sergiorbf <sbittencourt19@gmail.com>
2023-07-17 19:58:13 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import styles from "./App.module.css";
+import "./global.css";
+import { Header } from './header';
+
+export function App() {
+
+  return (
+    <div>
+      <Header/>
+    </div>
+  )
+}
+
+export default App
